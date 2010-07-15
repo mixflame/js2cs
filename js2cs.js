ccb4e746717947c9acf613076fa57518d57a9ea6
Jon <jon@jsnetbook.jolicloud-domain>
2010-07-15 01:18:32 -0700

    it can compile itself... although buggily

--- js2cs.js
+++ js2cs.js
@@ -8,10 +8,14 @@ var fs = require('fs');
 var p = function(obj) { sys.puts(sys.inspect(obj, true, 100)); }
 
 /* the missing trim method */
-String.prototype.trim = function () { return this.replace(/^\s*/, "").replace(/\s*$/, ""); }
+String.prototype.trim = function ()
+{
+return this.replace(/^\s*/, "").replace(/\s*$/, "");
+}
 
 /* read input (sync) */
-try {
+try
+{
 var string_raw_js = fs.readFileSync(process.argv[2], "utf8");
 } catch(e) {
 sys.log("Failed to read input file.. Did you specify one?");
@@ -54,7 +58,10 @@ var parseChildNodes = function(nodes) {
         is_last_statement = (i < nodes.length -1);
         is_just_var = (is_last_statement && (_node.type == "Variable")); /* variables are not declared this way in coffee */
         is_break = (_node.type == "BreakStatement"); /* not used in coffee */
-        if (!(is_just_var) && !(is_break)) parseNode(_node);
+        if (!(is_just_var) && !(is_break))
+        {
+        parseNode(_node);
+        }
         /* it doesnt wake up from the above until the line is over */
         if (is_last_statement && !(is_break) && !(is_just_var))
         { 
@@ -78,33 +85,48 @@ var parseNode = function(node) {
     sys.puts(iteration + " (" + indent_level + ") " +  node.type + " - " + node.name);
   } 
 
-  switch (node.type) {
-    case "Program":
-      if (node.elements) { parseChildNodes(node.elements); }
+  switch(node.type)
+  {
+    case("Program"):
+      if (node.elements)
+      {
+      parseChildNodes(node.elements);
+      }
       break;
-    case "This":
+    case("This"):
       addToOut("@");
       break;
-    case "Function":
-      if (node.params.length > 0) {
+    case("Function"):
+      if (node.params.length > 0)
+      {
         addToOut("(");
-        for(var i = 0; i < node.params.length; ++i) {
+        for(var i = 0; i < node.params.length; ++i)
+        {
           addToOut(node.params[i]);
-          if (i < node.params.length - 1) addToOut(", ");
+          if (i < node.params.length - 1) 
+          {
+          addToOut(", ");
+          }
         }
         addToOut(")");
       }
       addToOut(" ->\n");
       increaseIndent();
-      if (node.elements) { parseChildNodes(node.elements); }
+      if (node.elements)
+      {
+      parseChildNodes(node.elements);
+      }
       decreaseIndent();
       break;
-    case "Block":
+    case("Block"):
       increaseIndent();
-      if (node.statements) { parseChildNodes(node.statements); }
+      if (node.statements)
+      {
+      parseChildNodes(node.statements);
+      }
       decreaseIndent();
       break;
-    case "SwitchStatement":
+    case("SwitchStatement"):
       addToOut("switch ");
       parseNode(node.expression);
       addToOut("\n");
@@ -112,50 +134,64 @@ var parseNode = function(node) {
       parseChildNodes(node.clauses);
       decreaseIndent();
       break;
-    case "CaseClause":
+    case("CaseClause"):
       addToOut("when ");
       parseNode(node.selector);
       /* 2 is the minimum because break; is a statement too */
-      if (node.statements.length > 2 || node.statements.length == 1)
+      if ((node.statements.length > 2) || (node.statements.length == 1))
       {
         addToOut("\n");
         increaseIndent();
-        if (node.statements) parseChildNodes(node.statements);
+        if (node.statements)
+        {
+        parseChildNodes(node.statements);
+        }
         decreaseIndent();
       }
       else if (node.statements.length == 2)
       {
         addToOut(" then ");
-        if (node.statements) parseNode(node.statements[0]);
+        if (node.statements) 
+        {
+        parseNode(node.statements[0]);
+        }
       }
 
       break;
-    case "DefaultClause":
+    case("DefaultClause"):
       addToOut("else ");
-      if (node.statements.length > 2 || node.statements.length == 1)
+      if ((node.statements.length > 2) || (node.statements.length == 1))
       {
         addToOut("\n");
         increaseIndent();
-        if (node.statements) parseChildNodes(node.statements);
+        if (node.statements) 
+        {
+        parseChildNodes(node.statements);
+        }
         decreaseIndent();
       }
       else if (node.statements.length == 2)
       {
         addToOut(" then ");
-        if (node.statements) parseNode(node.statements[0]);
+        if (node.statements) 
+        {
+        parseNode(node.statements[0]);
+        }
       }
       break;
-    case "BreakStatement":
+    case("BreakStatement"):
       break;
-    case "LabelledStatement":
+    case("LabelledStatement"):
       break;
-    case "IfStatement":
+    case("IfStatement"):
       /* condition */
       if(node.condition.operator != "!")
       {
         addToOut("if ");
         parseNode(node.condition);
-      } else {
+      }
+      else
+      {
         addToOut("unless ");
         /* skip next node, it's "not" */
         parseNode(node.condition.expression);
@@ -163,18 +199,24 @@ var parseNode = function(node) {
       addToOut("\n");
       /* statements */
       increaseIndent();
-      if (node.ifStatement.statements) { parseChildNodes(node.ifStatement.statements); }
+      if (node.ifStatement.statements) 
+      {
+      parseChildNodes(node.ifStatement.statements);
+      }
       decreaseIndent();
       if(node.elseStatement != null) {
       addToOut("\n");
       addToOut("else"); /* limitation: javascript.pegjs doesnt know else if */
       addToOut("\n");
       increaseIndent();
-      if (node.elseStatement.statements) { parseChildNodes(node.elseStatement.statements); }
+      if (node.elseStatement.statements) 
+      {
+      parseChildNodes(node.elseStatement.statements);
+      }
       decreaseIndent();
       }
       break;
-    case "ForStatement":
+    case("ForStatement"):
       /* converts to while because this mode is unsupported */
       parseNode(node.initializer);
       addToOut("\n");
@@ -186,13 +228,13 @@ var parseNode = function(node) {
       decreaseIndent();
       if(node.statement) parseNode(node.statement);
       break;
-    case "WhileStatement":
+    case("WhileStatement"):
       addToOut("while ");
       parseNode(node.condition);
       addToOut("\n");
       if(node.statement) parseNode(node.statement);
       break;
-    case "TryStatement":
+    case("TryStatement"):
       addToOut("try\n");
       parseNode(node.block);
       addToOut("\n");
@@ -205,53 +247,56 @@ var parseNode = function(node) {
       parseNode(node['finally']);
       }
       break;
-    case "Catch":
-      if (node.identifier) addToOut(node.identifier);
+    case("Catch"):
+      if (node.identifier) 
+      {
+      addToOut(node.identifier);
+      }
       addToOut("\n");
       parseNode(node.block);
       addToOut("\n");
       break;
-    case "Finally":
+    case("Finally"):
       parseNode(node.block);
       break;
-    case "AssignmentExpression":
+    case("AssignmentExpression"):
       parseNode(node.left);
       addToOut(": ");
       parseNode(node.right);
       break;
-    case 'PropertyAssignment':
+    case("PropertyAssignment"):
       addToOut(node.name);
       addToOut(": ");
       parseNode(node.value);
       break;
-    case "PropertyAccess":
+    case("PropertyAccess"):
       parseNode(node.base);
       if(node.base.type != "This") addToOut(".");
       /* addToOut(node.name.trim()); */
       parseNode(node.name);
       break;
-    case "BinaryExpression":
+    case("BinaryExpression"):
       parseNode(node.left);
       addToOut(" ");
-      switch (node.operator)
+      switch(node.operator)
       {
       /* switch to "not" and "isnt" or something here */
-      case "!":
+      case("!"):
         addToOut("not ");
         break;
-      case "===":
+      case("==="):
         addToOut("is ");
         break;
-      case "!==":
+      case("!=="):
         addToOut("isnt ");
         break;
-      case "&&":
+      case("&&"):
         addToOut("and ");
         break;
-      case "||":
+      case("||"):
         addToOut("or ");
         break;   
-      case ",":
+      case(","):
         addToOut("\n"); /* no support for that operator yet. try to evaluate on seperate lines. */
         break;   
       default:
@@ -260,10 +305,10 @@ var parseNode = function(node) {
       }
       parseNode(node.right);
       break;
-    case "UnaryExpression":
-      switch (node.operator)
+    case("UnaryExpression"):
+      switch(node.operator)
       {
-        case '!':
+        case('!'):
           addToOut("not ");
           break;
         default:
@@ -271,7 +316,7 @@ var parseNode = function(node) {
       }
       parseNode(node.expression);
       break;
-    case "ConditionalExpression":
+    case("ConditionalExpression"):
       addToOut("if ");
       parseNode(node.condition);
       addToOut(" ");
@@ -279,16 +324,16 @@ var parseNode = function(node) {
       addToOut(" else ");
       parseNode(node.falseExpression);
       break;
-    case "PostfixExpression":
-      switch (node.operator)
+    case("PostfixExpression"):
+      switch(node.operator)
       { 
-        case '++':
+        case('++'):
         parseNode(node.expression);
         addToOut(" = ");
         parseNode(node.expression);
         addToOut(" + 1");
         break;
-        case '--':
+        case('--'):
         parseNode(node.expression);
         addToOut(" = ");
         parseNode(node.expression);
@@ -297,67 +342,85 @@ var parseNode = function(node) {
       }
       addToOut("\n");
       break;
-    case "Variable":
-      if (node.name.indexOf("var") == -1) {
+    case("Variable"):
+      if ((node.name.indexOf("var") == -1))
+      {
         addToOut(node.name.trim());
-      } else {
+      }
+      else if ((node.name.indexOf("var") != -1))
+      {
         /* -5 because of 4 for "var " and 1 for " " after */
         addToOut(node.name.substr(4, node.name.length - 4).trim());
       }
       break;
-    case "FunctionCall":
+    case("FunctionCall"):
       parseNode(node.name);    
       if (node.arguments.length > 0)
       {
         addToOut(" ");
-        for(var i = 0; i < node.arguments.length; ++i) {
+        for(var i = 0; i < node.arguments.length; ++i)
+        {
           parseNode(node.arguments[i]);
-          if (i < node.arguments.length - 1) addToOut(", ");
+          if (i < node.arguments.length - 1) 
+          { 
+          addToOut(", ");
+          }
         }      
       }
       break;
-    case 'StringLiteral':
+    case('StringLiteral'):
       addToOut("'" + node.value + "'");
       break;
-    case 'NumericLiteral':
+    case('NumericLiteral'):
       addToOut(node.value);
       break;
-    case 'RegularExpressionLiteral':
+    case('RegularExpressionLiteral'):
       addToOut("/");
       addToOut(node.body);
       addToOut("/" + node.flags);
       break;
-    case 'NullLiteral':
+    case('NullLiteral'):
       addToOut("null");
       break;
-    case 'ArrayLiteral':
-      if (node.elements.length > 0) {
+    case('ArrayLiteral'):
+      if (node.elements.length > 0)
+      {
         addToOut("[");
-        for(var i = 0; i < node.elements.length; ++i) {
+        for(var i = 0; i < node.elements.length; ++i)
+        {
           parseNode(node.elements[i]);
-          if (i < node.elements.length - 1) addToOut(", ");
+          if (i < node.elements.length - 1) 
+          {
+          addToOut(", ");
+          }
         }
         addToOut("]");
       }
       break;
-    case 'ObjectLiteral':
-      if (node.properties.length > 0) {
+    case('ObjectLiteral'):
+      if (node.properties.length > 0)
+      {
         addToOut("{\n");
         increaseIndent();
-        if (node.properties) parseChildNodes(node.properties);
+        if (node.properties) 
+        {
+        parseChildNodes(node.properties);
+        }
         decreaseIndent();
         addToOut("\n}");
       }
       break;
-    case 'BooleanLiteral':
-      if (node.value == true) {
+    case('BooleanLiteral'):
+      if (node.value == true)
+      {
         addToOut("yes");
-      } else {
+      }
+      else if (node.value == false)
+      {
         addToOut("no");
       }
     break;      
   }
-  
 }
 
 
